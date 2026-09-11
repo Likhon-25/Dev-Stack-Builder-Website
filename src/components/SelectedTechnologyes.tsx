b42f4f8367743aab4bb7bf91d13320ab7330aa44
Likhon-25 <likhonmondol327@gmail.com>
2026-09-11 15:15:11 +0600

    your stack section complete and some bugs solved

--- src/components/SelectedTechnologyes.tsx
+++ src/components/SelectedTechnologyes.tsx
@@ -1,7 +1,7 @@
 import type { Dispatch, SetStateAction } from "react";
 import type { ITechnology } from "../types/Type";
-import { FaRegTrashAlt } from "react-icons/fa";
 import SelectedTechnologyCard from "./SelectedTechnologyCard";
+
 interface ISelectedTechnologyesProp {
   selectedTechnologies: ITechnology[];
   setSelectedTechnologies: Dispatch<SetStateAction<ITechnology[]>>;
@@ -23,25 +23,42 @@ const SelectedTechnologyes = ({
   };
 
   return (
-    <div className="rounded-2xl border border-slate-200 bg-white p-6 shadow-sm">
+    <div className="w-80 rounded-2xl border border-slate-200 bg-white p-6 shadow-sm">
       <h2 className="text-lg font-bold text-slate-900">Your Stack</h2>
+
       <p className="mt-1 text-xs font-medium text-slate-400">
-        {selectedTechnologies.length} Technology Selected
+        {selectedTechnologies.length === 0
+          ? "No technologies selected yet."
+          : `${selectedTechnologies.length} Technology Selected`}
       </p>
 
       <div className="mt-5 space-y-3">
-        {selectedTechnologies.map((selectTech) => (
-          <SelectedTechnologyCard selectTech={selectTech} handleReomveTech={handleReomveTech}  />
-        ))}
+        {selectedTechnologies.length === 0 ? (
+          <div className="flex h-24 items-center justify-center rounded-2xl border border-dashed border-slate-200">
+            <p className="text-xs font-medium text-slate-400">
+              Your stack is empty.
+            </p>
+          </div>
+        ) : (
+          selectedTechnologies.map((selectTech) => (
+            <SelectedTechnologyCard
+              key={selectTech.id}
+              selectTech={selectTech}
+              handleReomveTech={handleReomveTech}
+            />
+          ))
+        )}
       </div>
 
-      <button
-        onClick={() => handleRemoveAll()}
-        className="mt-6 flex w-full items-center justify-center gap-4 rounded-xl border border-red-500 bg-white py-3 text-xs font-semibold text-red-500  cursor-pointer"
-      >
-        <span>Remove All</span>
-        <FaRegTrashAlt className="text-sm" />
-      </button>
+      {selectedTechnologies.length != 0 && (
+        <button
+          type="button"
+          onClick={handleRemoveAll}
+          className="mt-6 w-full rounded-xl border border-red-500 bg-white py-3 text-xs font-semibold text-red-500 transition-colors hover:bg-rose-50 cursor-pointer"
+        >
+          Remove All
+        </button>
+      )}
     </div>
   );
 };
